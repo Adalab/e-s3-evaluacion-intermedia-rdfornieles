c4f2688ba501aeb8295858a2ed76c1a1852c63c6
rdfornieles <rdfornieles@gmail.com>
2019-01-14 15:55:42 +0100

    done PokeList component

--- src/Components/PokeList.js
+++ src/Components/PokeList.js
@@ -1,1 +1,29 @@
 import React, { Component } from 'react';
+import Pokemon from './Pokemon';
+
+class PokeList extends Component {
+    render() {
+        return (
+            <ul>
+               {this.props.pokemonArr.map((item, index) =>
+                <li key = {index}>
+                <Pokemon 
+                name = {item.name}
+                types = {item.types.map((types, i) =>
+                    <ul key ={i}>
+                    <li>
+                        {types}
+                    </li>
+                    </ul>
+                    )}
+                photo = {item.url}
+                />
+                </li>
+
+                )}
+            </ul>
+        )
+    }
+}
+
+export default PokeList;
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,1 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Pokemon extends Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <ul>
+                    <li>{this.props.types}</li>
+                </ul>
+            
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
+export default Pokemon;
